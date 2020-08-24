60b89ce3526ad15c2aff899b12f59b79d3b4b62c
moo1323@naver.com <moo1323@naver.com>
2020-08-25 07:17:44 +0900

    propTypes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ name, picture }) {
+const pictureILike = [
+  {
+    id: 1,
+    name: "paintedGirl",
+    picture:
+      "https://images.unsplash.com/photo-1503454537195-1dcabb73ffb9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    rating: 5
+  },
+  {
+    id: 2,
+    name: "braveBoy",
+    picture:
+      "https://images.unsplash.com/photo-1519238263530-99bdd11df2ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    rating: 4
+  },
+  {
+    id: 3,
+    name: "hikingBoy",
+    picture:
+      "https://images.unsplash.com/photo-1503919545889-aef636e10ad4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    rating: 4.3
+  }
+];
+
+function Pictures({ name, picture, rating }) {
   return (
     <div>
       <h1>I like {name}</h1>
-      <img src={picture} />
+      <h4>rating: {rating}</h4>
+      <img src={picture} alt={name} />
     </div>
   );
 }
 
-const pictureILike = [
-  {
-    name: "paintedGirl",
-    picture:
-      "https://images.unsplash.com/photo-1503454537195-1dcabb73ffb9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-  },
-  {
-    name: "braveBoy",
-    picture:
-      "https://images.unsplash.com/photo-1519238263530-99bdd11df2ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-  },
-  {
-    name: "hikingBoy",
-    picture:
-      "https://images.unsplash.com/photo-1503919545889-aef636e10ad4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-  }
-];
+Pictures.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 function App() {
   return (
     <div>
       {pictureILike.map(info => (
-        <Food name={info.name} picture={info.picture} />
+        <Pictures
+          key={info.id}
+          name={info.name}
+          picture={info.picture}
+          rating={info.rating}
+        />
       ))}
     </div>
   );
