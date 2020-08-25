77a414f8dbde146bfe7d48d4f90f98a13bfceab0
moo1323@naver.com <moo1323@naver.com>
2020-08-25 18:57:34 +0900

    Updates

---
+++ precache-manifest.a63604904e8703128a36b9c516d7fd98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9057e097ae43c0228ca2f90920157a04",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "7ff7743f8926293f8646",
+    "url": "/movie_app/static/css/main.2205ef7b.chunk.css"
+  },
+  {
+    "revision": "b9cb88537a0f4167021b",
+    "url": "/movie_app/static/js/2.1c14cd22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.1c14cd22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ff7743f8926293f8646",
+    "url": "/movie_app/static/js/main.0c52848a.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
